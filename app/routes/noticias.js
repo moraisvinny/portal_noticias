a7d1fe151cd8b37627a56106d0fd97b11a6386c6
Vinicius Morais <vinicius.reis.morais@gmail.com>
2017-11-28 18:53:53 -0200

    auto load da config / conexao banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,10 +1,10 @@
-var dbConnection = require('../../config/db-connection');
+
 module.exports = function(app) {
     
     app.get('/noticias', function(req, res){
         
-        var connection = dbConnection();
-        
+        var connection = app.config.dbConnection();
+
         connection.query('select * from noticias', function(error, result){
             res.render("noticias/noticias", {noticias: result});
         });
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function() {
-
+var connMYSQL = function () {
+    console.log("Conexão com bd estabelecida");
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '1234',
         database: 'portal_noticias'
     });
+};
+module.exports = function () {
+    console.log("Exportado Modulo de conexao");
+    return connMYSQL;
+
 };
